d02095dcaf6bcb92642a2f0536ec3693b2e253ca
James Quigley <james@quigley.us>
2017-01-08 17:03:14 -0700

    End point functions to support forms

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,10 @@
+var lang = "ar";
 $(document).ready(function () {
     console.log("done");
 });
 
 $("#shorten").click(function () {
-    $.getJSON("/shorten/" + $("#longUrl").val(), function (data) {
-        console.log(data.id);
-        $("#shortUrl").text(data.id);
-        $("#shortUrlLink").attr("href", data.id);
-    });
+    shortenUrl();
 });
 
 $("#translate").click(function () {
@@ -15,7 +12,20 @@ $("#translate").click(function () {
 });
 
 function translatePhrase(){
-    $.getJSON("/translate/" + $("#langs").val() + "/" + $("#source").val(), function (body) {
+    $.getJSON("/translate/" + lang + "/" + $("#source").val(), function (body) {
         $("#dest").val($.parseJSON(body).data.translations[0].translatedText);
     });
+};
+
+function shortenUrl() {
+    $.getJSON("/shorten/" + $("#longUrl").val(), function (data) {
+        console.log(data.id);
+        $("#shortUrl").val(data.id);
+        $("#shortUrlLink").attr("href", data.id);
+    });
+};
+
+function setLang(language, langShort){
+    lang = langShort;
+    $("#langs").text(language + " \u25bc");
 };
